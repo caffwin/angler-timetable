0a60d68c8c7dae350390d0b8232be5377256a07e
caffwin <catheralligator@gmail.com>
2019-08-27 02:28:03 -0700

    Working weather detector, need to add timestamps to each weather change

--- weather_detector.py
+++ weather_detector.py
@@ -15,3 +15,51 @@
 
 # Approach:
 # Basic window slider algorithm
+
+# Notes:
+# First and second can be equal to eachother, the same weather
+
+
+def find_weather_pattern(weather_list, first, second):
+
+    # Initialize sequence to check
+    len_list = len(weather_list)
+    match_this_window = [first, second]
+    window_string = match_this_window[0] + " > " + match_this_window[1]
+
+    print("Window to match is: ", window_string)
+
+    # Loop through each 
+    for i in range(0, len_list - 1):
+        # Create new list to hold test window
+        # Append first element, list[i], to test_window
+        # Append second element, list[i+1], to test_window
+        test_window = []
+        test_window.append(weather_list[i])
+        test_window.append(weather_list[i + 1])
+
+        # Check if they are equal
+        if test_window == match_this_window:
+            print("Window match found! " + window_string)
+
+    return 
+    # Continue going through the entire list - the test window may occur more than once
+
+
+    # If the slider window matches the current window, print out a notification and continue
+    # traversing until the end of the list
+
+
+
+
+
+find_weather_pattern(["Clouds", "Fair Skies", "Clear", "Clear", "Thunder", "Clear", "Fair Skies", "Fog"], "Thunder", "Clear")
+
+
+# Next step: change list elements into tuples instead of strings, so that each weather condition also 
+# includes the time (in game time) when the weather change occurs
+
+
+
+# Eventually: 
+# Figure out how to pass in-game time and weather patterns from client-side to server
