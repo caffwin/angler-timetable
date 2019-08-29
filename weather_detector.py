4855cf5dfb5ebd7610e3675fe03456b4ba7d87c3
Catherine Li <catheralligator@gmail.com>
2019-08-29 00:00:53 -0700

    Time stamps added, slider algorithm prints start time for specific weather patterns

--- weather_detector.py
+++ weather_detector.py
@@ -27,20 +27,24 @@ def find_weather_pattern(weather_list, first, second):
     match_this_window = [first, second]
     window_string = match_this_window[0] + " > " + match_this_window[1]
 
-    print("Window to match is: ", window_string)
+    print("Window to match is: " + window_string)
 
     # Loop through each 
     for i in range(0, len_list - 1):
+        # print("Entered for loop!")
         # Create new list to hold test window
         # Append first element, list[i], to test_window
         # Append second element, list[i+1], to test_window
         test_window = []
-        test_window.append(weather_list[i])
-        test_window.append(weather_list[i + 1])
-
+        test_window.append(weather_list[i][0])
+        # print("Weather_list at index...", weather_list[i])
+        test_window.append(weather_list[i + 1][0])
+        print("Test window is: ", test_window)
         # Check if they are equal
+
         if test_window == match_this_window:
             print("Window match found! " + window_string)
+            print("Window time begins at (in-game time): " + weather_list[i][1])
 
     return 
     # Continue going through the entire list - the test window may occur more than once
@@ -53,7 +57,25 @@ def find_weather_pattern(weather_list, first, second):
 
 
 
-find_weather_pattern(["Clouds", "Fair Skies", "Clear", "Clear", "Thunder", "Clear", "Fair Skies", "Fog"], "Thunder", "Clear")
+# find_weather_pattern(["Clouds", "Fair Skies", "Clear", "Clear", "Thunder", "Clear", "Fair Skies", "Fog"], "Thunder", "Clear")
+
+
+w1 = ("Clouds", "00:00")
+w2 = ("Fair Skies", "08:00")
+w3 = ("Clear", "16:00")
+w4 = ("Clear", "00:00")
+w5 = ("Thunder", "8:00")
+w6 = ("Clear", "16:00")
+w7 = ("Fair Skies", "00:00")
+w8 = ("Fog", "8:00")
+
+weather_time_list = [w1, w2, w3, w4, w5, w6, w7, w8]
+print("Weather time list is: ", weather_time_list)
+print(weather_time_list[0][0])
+
+
+print("Second try: ")
+find_weather_pattern(weather_time_list, "Thunder", "Clear")
 
 
 # Next step: change list elements into tuples instead of strings, so that each weather condition also 
